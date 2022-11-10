2a981b61351b246ad642ef66c6fc9c2f72cdd670
Philip <PhilipH3ro@gmail.com>
2022-11-10 10:58:56 +0100

    OnPostTurnOffElevator Base

--- Grupp3-AdminApp/Pages/Elevator/ElevatorDetails.cshtml.cs
+++ Grupp3-AdminApp/Pages/Elevator/ElevatorDetails.cshtml.cs
@@ -5,6 +5,7 @@ using Grupp3_Elevator.Services.Errand;
 using Grupp3_Elevator.Services.Technician;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.Devices;
 using Microsoft.EntityFrameworkCore;
 
 namespace Grupp3_Elevator.Pages.Elevator
@@ -28,6 +29,21 @@ namespace Grupp3_Elevator.Pages.Elevator
             Elevator = await _elevatorService.GetElevatorDeviceByIdAsync(elevatorId);
 
             Errands = _errandService.GetErrandsFromElevatorId(elevatorId);
+        }
+        public async Task<IActionResult> OnPostTurnOffElevator(string elevatorId)
+        {
+            try
+            {
+                using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("HostName=kyh-shared-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=/5asl5agNK3raYZNyfkumb0vcsnT+OdUeoUOupOWLQo=");
+
+                var directMethod = new CloudToDeviceMethod("TurnOffElevatorDM");
+                var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
+            }
+            catch
+            {
+
+            }
+            return RedirectToPage("ElevatorDetails", new { elevatorId = elevatorId });
         }
     }
 }
