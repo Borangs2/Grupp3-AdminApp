07b4aabb49cc6ba0bf2d3432d90be85fd59de2e2
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-11-10 14:21:20 +0100

    Trying with changeLevel

--- Grupp3-AdminApp/Pages/Elevator/ElevatorDetails.cshtml.cs
+++ Grupp3-AdminApp/Pages/Elevator/ElevatorDetails.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Azure.Devices;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Grupp3_Elevator.Pages.Elevator
 {
@@ -33,12 +34,61 @@ namespace Grupp3_Elevator.Pages.Elevator
             Errands = _errandService.GetErrandsFromElevatorId(elevatorId);
         }
 
+        public async Task<IActionResult> OnPostChangeLevel(string elevatorId, int targetLevel)
+        {
+            try
+            {
+                using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(_configuration.GetValue<string>("IoTHubConnection"));
+                var directMethod = new CloudToDeviceMethod("ChangeLevel");
+                directMethod.SetPayloadJson(JsonConvert.SerializeObject(targetLevel));
+                var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
+            }
+            catch { }
+            return RedirectToPage("ElevatorDetails", new { elevatorId = elevatorId });
+        }
+
+        public async Task<IActionResult> OnPostOpenDoors(string elevatorId)
+        {
+            try
+            {
+                using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(_configuration.GetValue<string>("IoTHubConnection"));
+                var directMethod = new CloudToDeviceMethod("OpenDoors");
+                var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
+            }
+            catch { }
+            return RedirectToPage("ElevatorDetails", new { elevatorId = elevatorId });
+        }
+
+        public async Task<IActionResult> OnPostCloseDoors(string elevatorId)
+        {
+            try
+            {
+                using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(_configuration.GetValue<string>("IoTHubConnection"));
+                var directMethod = new CloudToDeviceMethod("CloseDoors");
+                var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
+            }
+            catch { }
+            return RedirectToPage("ElevatorDetails", new { elevatorId = elevatorId });
+        }
+
+        public async Task<IActionResult> OnPostResetElevator(string elevatorId)
+        {
+            try
+            {
+                using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(_configuration.GetValue<string>("IoTHubConnection"));
+                var directMethod = new CloudToDeviceMethod("ResetElevator");
+                var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
+            }
+            catch { }
+            return RedirectToPage("ElevatorDetails", new { elevatorId = elevatorId });
+        }
+
         public async Task<IActionResult> OnPostTurnOnElevator(string elevatorId)
         {
             try
             {
                 using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(_configuration.GetValue<string>("IoTHubConnection"));
-                var directMethod = new CloudToDeviceMethod("TurnOnElevatorDM");
+                var directMethod = new CloudToDeviceMethod("TurnOnElevator");
                 var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
             }
             catch { }
@@ -50,7 +100,7 @@ namespace Grupp3_Elevator.Pages.Elevator
             try
             {
                 using ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(_configuration.GetValue<string>("IoTHubConnection"));
-                var directMethod = new CloudToDeviceMethod("TurnOffElevatorDM");
+                var directMethod = new CloudToDeviceMethod("TurnOffElevator");
                 var result = await serviceClient.InvokeDeviceMethodAsync(elevatorId, directMethod);
             }
             catch { }
