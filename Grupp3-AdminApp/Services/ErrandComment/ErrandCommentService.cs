de9a712e51a83d3409db54bc42beb67a5488d244
Andreas Boräng <andbor02@gmail.com>
2022-11-14 13:01:48 +0100

    Re-fixed unmerged changes

--- Grupp3-AdminApp/Services/ErrandComment/ErrandCommentService.cs
+++ Grupp3-AdminApp/Services/ErrandComment/ErrandCommentService.cs
@@ -13,20 +13,36 @@ namespace Grupp3_AdminApp.Services.ErrandComment
             _context = context;
         }
 
-        public ErrandCommentModel GetCommentsById(string commentId)
+        public async Task<ErrandCommentModel> GetErrandCommentsByIdAsync(string commentId)
         {
             return _context.ErrandComments.FirstOrDefault(e => e.Id == Guid.Parse(commentId));
         }
-       
-        public List<ErrandCommentModel> GetComments()
+
+        public async Task<List<ErrandCommentModel>> GetErrandCommentsAsync()
         {
             return _context.ErrandComments.ToList();
         }
 
-        public List<ErrandCommentModel> GetErrandCommentsFromErrandId(string errandId)
+        public async Task<List<ErrandCommentModel>> GetErrandCommentsFromErrandIdAsync(string errandId)
         {
             var result = _context.Errands.Include(e => e.Comments).FirstOrDefault(e => e.Id == Guid.Parse(errandId));
             return result.Comments;
+        }
+        public async Task<ErrandCommentModel> CreateErrandCommentAsync(ErrandModel errand, string chosenSelectTechnician, string content)
+        {
+            var ErrandComment = new ErrandCommentModel
+            {
+                Id = Guid.NewGuid(),
+                Content = content,
+                Author = Guid.Parse(chosenSelectTechnician),
+                PostedAt = DateTime.Now
+            };
+            errand.Comments.Add(ErrandComment);
+            _context.Entry(ErrandComment).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+
+            ErrandCommentModel result = await GetErrandCommentsByIdAsync(ErrandComment.Id.ToString());
+            return result;
         }
     }
 }
--- Grupp3-AdminApp/Services/ErrandComment/IErrandCommentService.cs
+++ Grupp3-AdminApp/Services/ErrandComment/IErrandCommentService.cs
@@ -4,10 +4,9 @@ namespace Grupp3_AdminApp.Services.ErrandComment
 {
     public interface IErrandCommentService
     {
-        ErrandCommentModel GetCommentsById(string commentId);
-        List<ErrandCommentModel> GetErrandCommentsFromErrandId(string errandId);
-
-        //ErrandCommentModel GetErrandCommentFromErrandId(string errandId);
-
+        Task<ErrandCommentModel> GetErrandCommentsByIdAsync(string commentId);
+        Task<List<ErrandCommentModel>> GetErrandCommentsAsync();
+        Task<List<ErrandCommentModel>> GetErrandCommentsFromErrandIdAsync(string errandId);
+        Task<ErrandCommentModel> CreateErrandCommentAsync(ErrandModel errand, string chosenSelectTechnician, string content);
     }
 }
