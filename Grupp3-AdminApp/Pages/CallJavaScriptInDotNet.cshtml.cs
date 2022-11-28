5f6028766bc7c3770106cbe1a9dd9ef494af961f
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-11-28 11:22:47 +0100

    starting

---
+++ Grupp3-AdminApp/Pages/CallJavaScriptInDotNet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grupp3_AdminApp.Pages
+{
+    public partial class CallJavaScriptInDotNetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
