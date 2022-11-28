6fa8d465025f3b80266aacb26b6acbf9e211c217
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-11-28 15:35:14 +0100

    Not working at all

--- Grupp3-AdminApp/Pages/Index.cshtml.cs
+++ Grupp3-AdminApp/Pages/Index.cshtml.cs
@@ -1,6 +1,11 @@
 ﻿using Grupp3_Elevator.Data;
+using Grupp3_Elevator.Models;
+using Grupp3_Elevator.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Forms.DataVisualization.Charting;
+using static MudBlazor.CategoryTypes;
 
 namespace Grupp3_Elevator.Pages
 {
@@ -8,8 +13,7 @@ namespace Grupp3_Elevator.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _context;
-
-
+        private readonly IElevatorService _elevatorService;
         public IndexModel(ApplicationDbContext context, ILogger<IndexModel> logger)
         {
             _context = context;
@@ -19,14 +23,32 @@ namespace Grupp3_Elevator.Pages
         public string ElevatorsAmount { get; set; }
         public string TechnichansAmount { get; set; }
         public string CommentsAmount { get; set; }
+        public List<int> ErrandsPerElevatorOne { get; set; }
 
-        public void OnGet()
+        public List<ElevatorDeviceItem> Elevators { get; set; }
+        public List<int> Errands { get; set; }
+
+        public async Task OnGet()
         {
             ErrandsAmount = _context.Errands.Select(a => a.Id).Count().ToString();
             ElevatorsAmount = _context.Elevators.Select(a => a.Id).Count().ToString();
 
             TechnichansAmount = _context.Technicians.Select(a => a.Id).Count().ToString();
             CommentsAmount = _context.ErrandComments.Select(a => a).Count().ToString();
+
+            Elevators = await _elevatorService.GetElevatorsAsync();
+
+
+
+            Errands = _context.Elevators.Include(e => e.Errands).Select(e => e.Errands.Count()).ToList();
+
+            var myChart = new Chart(width: 600, height: 400)
+                .AddTitle("Errands for every elevator")
+                .AddSeries("Default",
+                xValue: Model.Elevators.Select(n => n.Name), xField: "Name",
+                yValues: Model.Errands, yFields: "Errands")
+                .Write();
+
         }
     }
 }
